2e0ec1a05eab8bdfe8f7707bdf82b3c591933864
michelefitstic <michele.francia.studio@fitstic-edu.com>
2024-04-12 15:54:05 +0200

    Added SplashScreen, added splash logos to assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:triptaptoe_app/screens/splash_screen.dart';
 
 import 'screens/home_screen.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme:
           ThemeData.light().copyWith(textTheme: GoogleFonts.poppinsTextTheme()),
       home: SafeArea(child: HomeScreen())));
